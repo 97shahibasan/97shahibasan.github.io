fd5d6343a61c0b740240e4facd41c0eb852496e5
Basan Shahi <97shahibasan@gmail.com>
2025-11-10 15:19:43 +0530

    Delete HTML directory

--- HTML/style.js
+++
@@ -1,8 +0,0 @@
-/* Navbar scroll effect */
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 50) {
-        $('.navbar-custom').addClass('scrolled');
-    } else {
-        $('.navbar-custom').removeClass('scrolled');
-    }
-});
